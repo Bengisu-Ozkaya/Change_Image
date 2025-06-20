cf0c2d2930cc25cf9e3eb9cd21e25e5f299e93f0
bngs <162094351+Bengisu-Ozkaya@users.noreply.github.com>
2025-06-21 01:35:33 +0300

    Change_Image
    Butona basıınca fotoğraf değişiyor

---
+++ Kotlin/app/src/main/java/com/example/kotlin/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.kotlin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void changeImage(View view){
+        if (i == 0) {
+            ImageView img = findViewById(R.id.imageView);
+            img.setImageResource(R.drawable.b); // R = resources
+            i++;
+        }else{
+            ImageView img = findViewById(R.id.imageView);
+            img.setImageResource(R.drawable.a);
+            i--;
+        }
+
+    }
+}
